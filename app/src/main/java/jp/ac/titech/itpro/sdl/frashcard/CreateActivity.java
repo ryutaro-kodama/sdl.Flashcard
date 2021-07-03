19e9423a44dba61f79652782722d0660ace02f97
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-07-03 13:57:37 +0900

    Create save method on 'CreateActivity'.

--- app/src/main/java/jp/ac/titech/itpro/sdl/frashcard/CreateActivity.java
+++ app/src/main/java/jp/ac/titech/itpro/sdl/frashcard/CreateActivity.java
@@ -2,6 +2,8 @@ package jp.ac.titech.itpro.sdl.frashcard;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -20,16 +22,19 @@ public class CreateActivity extends AppCompatActivity {
 
         final TextView createFrontTextView = (TextView) findViewById(R.id.create_front_text);
         createFrontTextView.setText(R.string.create_front_text);
-
         final TextView createBackTextView = (TextView) findViewById(R.id.create_back_text);
         createBackTextView.setText(R.string.create_back_text);
 
-//        TextView title = findViewById(R.id.create_title);
-//        title.setText(TAG);
-//
-//        String name = getIntent().getStringExtra(NAME_EXTRA);
-//        // 引数情報を取り出し
-//        TextView answer = findViewById(R.id.answer_answer);
-//        answer.setText(getString(R.string.answer_format, name));
+        Button saveButton = (Button)findViewById(R.id.create_save_button);
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                saveNewCard(v);
+            }
+        });
+    }
+
+    private void saveNewCard(View v) {
+        Log.d(TAG, "saveNewCard");
     }
 }
