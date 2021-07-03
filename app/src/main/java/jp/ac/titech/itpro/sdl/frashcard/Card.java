84206155ccd0e8e81298fe26f3807453f6b3847c
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-07-03 23:12:45 +0900

    Create load and save method to local json file.

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/frashcard/Card.java
@@ -0,0 +1,54 @@
+package jp.ac.titech.itpro.sdl.frashcard;
+
+import android.util.JsonWriter;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+public class Card {
+    public static final String FRONT = "front";
+    public static final String BACK_TRUE = "back_true";
+    public static final String BACK_FALSE1 = "back_false1";
+    public static final String BACK_FALSE2 = "back_false2";
+    private String front;
+    private String back_true;
+    private String back_false1;
+    private String back_false2;
+
+    public Card(String front, String back_true, String back_false1, String back_false2) {
+        this.front = front;
+        this.back_true = back_true;
+        this.back_false1 = back_false1;
+        this.back_false2 = back_false2;
+    }
+
+    public Card(String front, String back_true, String back_false1) {
+        new Card(front, back_true, back_false1, "");
+    }
+
+    public Card(String front, String back_true) {
+        new Card(front, back_true, "", "");
+    }
+
+    public void writeCard(JsonWriter writer) throws IOException {
+        writer.beginObject();
+        writer.name(FRONT).value(front);
+        writer.name(BACK_TRUE).value(back_true);
+        writer.name(BACK_FALSE1).value(back_false1);
+        writer.name(BACK_FALSE2).value(back_false2);
+        writer.endObject();
+    }
+
+    @Override
+    public String toString() {
+        return "Card{" +
+                "front='" + front + '\'' +
+                ", back_true='" + back_true + '\'' +
+                ", back_false1='" + back_false1 + '\'' +
+                ", back_false2='" + back_false2 + '\'' +
+                '}';
+    }
+}
