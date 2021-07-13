6a9f3b9cbb4fa104a042538e5937bc724b3c481d
ryutaro-kodama <slj-flz-m2@ezweb.ne.jp>
2021-07-13 12:11:43 +0900

    Rename project name

---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/flashcard/card/CardDataFile.java
@@ -0,0 +1,135 @@
+package jp.ac.titech.itpro.sdl.flashcard.card;
+
+import android.content.Context;
+import android.util.JsonReader;
+import android.util.JsonWriter;
+import android.util.Log;
+
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+
+import static jp.ac.titech.itpro.sdl.flashcard.card.Card.BACK_FALSE1;
+import static jp.ac.titech.itpro.sdl.flashcard.card.Card.BACK_FALSE2;
+import static jp.ac.titech.itpro.sdl.flashcard.card.Card.BACK_TRUE;
+import static jp.ac.titech.itpro.sdl.flashcard.card.Card.FRONT;
+
+public class CardDataFile {
+    private final static String TAG = CardDataFile.class.getSimpleName();
+    private final String FILENAME = "CardData.json";
+
+    private final File file;
+    private ArrayList<Card> cardData;
+
+    public CardDataFile(Context context){
+        // Set target file.
+        file = new File(context.getExternalFilesDir(""), FILENAME);
+
+        // Open data file if exists.
+        if (!file.exists()) {
+            cardData = new ArrayList<Card>();
+            return;
+        }
+
+        // Get saved data(try-with-resources).
+        ArrayList<String> lines = new ArrayList<>();
+        try( JsonReader reader = new JsonReader(new FileReader(file)) ){
+            cardData = read(reader);
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+            return;
+        }
+    }
+
+    private ArrayList<Card> read(JsonReader reader) throws IOException  {
+        ArrayList<Card> result = new ArrayList<>();
+
+        reader.beginArray();
+        while (reader.hasNext()) {
+            result.add(readCard(reader));
+        }
+        reader.endArray();
+
+        return result;
+    }
+
+    private Card readCard(JsonReader reader) throws IOException {
+        String front = "", back_true = "", back_false1 = "", back_false2 = "";
+
+        reader.beginObject();
+        while (reader.hasNext()) {
+            String name = reader.nextName();
+            if (name.equals(FRONT)) {
+                front = reader.nextString();
+            } else if (name.equals(BACK_TRUE)) {
+                back_true = reader.nextString();
+            } else if (name.equals(BACK_FALSE1)) {
+                back_false1 = reader.nextString();
+            } else if (name.equals(BACK_FALSE2)) {
+                back_false2 = reader.nextString();
+            } else {
+                reader.skipValue();
+            }
+        }
+        reader.endObject();
+
+        return new Card(front, back_true, back_false1, back_false2);
+    }
+
+    public void addAllTmpData(ArrayList<Card> tmpCardData) {
+        cardData.addAll(tmpCardData);
+    }
+
+    public ArrayList<Card> getCardData() {
+        return cardData;
+    }
+
+    public void save() {
+        // Open or create file.
+        if (!file.exists()) {
+            try {
+                Log.d(TAG, "getData - create new file at: " + file.toString());
+                file.createNewFile();
+            } catch (IOException e) {
+                e.printStackTrace();
+                return;
+            }
+        }
+
+        try ( JsonWriter writer = new JsonWriter(new FileWriter(file)) ) {
+            write(writer);
+        } catch (IOException e ) {
+            System.err.println( e.getMessage() );
+        }
+    }
+
+    private void write(JsonWriter writer) throws IOException {
+        writer.setIndent("  ");
+
+        writer.beginArray();
+        for (Card card: cardData){
+            card.writeCard(writer);
+        }
+        writer.endArray();
+
+        writer.close();
+    }
+
+    public void resetFile() {
+        if (file.exists()) {
+            file.delete();
+        }
+    }
+
+    public String toString() {
+        ArrayList<String> tmp = new ArrayList<>();
+
+        for (Card card: cardData) {
+            tmp.add(card.toString());
+        }
+        return tmp.toString();
+    }
+}
---
+++ app/src/main/java/jp/ac/titech/itpro/sdl/flashcard/test/TestMainActivity.java
@@ -0,0 +1,72 @@
+package jp.ac.titech.itpro.sdl.flashcard.test;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import jp.ac.titech.itpro.sdl.flashcard.R;
+import jp.ac.titech.itpro.sdl.flashcard.test.with_friend.TestWithFriendHomeActivity;
+
+// This is the activity for main test page.
+public class TestMainActivity extends AppCompatActivity {
+    private final static String TAG = TestMainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "onCreate");
+        setContentView(R.layout.activity_test_main);
+
+        // Set create button and register intent.
+        Button buttonTestFront = findViewById(R.id.test_front);
+        buttonTestFront.setOnClickListener(v -> {
+            Intent intent = new Intent(TestMainActivity.this, TestFrontActivity.class);
+            startActivity(intent);
+        });
+
+        Button buttonTestBack = findViewById(R.id.test_back);
+        buttonTestBack.setOnClickListener(v -> {
+            Intent intent = new Intent(TestMainActivity.this, TestBackActivity.class);
+            startActivity(intent);
+        });
+
+        Button buttonTestBackChoice = findViewById(R.id.test_back_from_choice);
+        buttonTestBackChoice.setOnClickListener(v -> {
+            Intent intent = new Intent(TestMainActivity.this, TestBackChoiceActivity.class);
+            startActivity(intent);
+        });
+
+        Button buttonTestWithFriend = findViewById(R.id.test_with_friend);
+        buttonTestWithFriend.setOnClickListener(v -> {
+            Intent intent = new Intent(TestMainActivity.this, TestWithFriendHomeActivity.class);
+            startActivity(intent);
+        });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
